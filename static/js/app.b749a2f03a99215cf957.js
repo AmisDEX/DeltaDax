6465c86af46e17992664f7d155c9b773cc8cd442
amisolution <siriusgem1@gmail.com>
2018-08-10 07:45:40 +0000

    Update app.b749a2f03a99215cf957.js

--- static/js/app.b749a2f03a99215cf957.js
+++ static/js/app.b749a2f03a99215cf957.js
@@ -6214,6 +6214,7 @@ webpackJsonp([1], {
             gasTrade: 25e4,
             gasOrder: 25e4,
             minOrderSize: .001,
+            ordersOnchain: true,
             apiServer: ["https://api.etherdelta.com"],
             socketServers: ["https://socket01.etherdelta.com", "https://socket02.etherdelta.com", "https://socket03.etherdelta.com", "https://socket04.etherdelta.com", "https://api.rektex.io"],
             userCookie: "EtherDelta",
